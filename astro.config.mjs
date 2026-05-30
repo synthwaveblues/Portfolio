ee381ee68913b5db14fd379534dee9cf6fc2d88d
synthwaveblues <synthwaveblues@gmail.com>
2026-05-30 12:52:19 +0200

    fix: updated site_url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-// TODO: replace with your actual domain once you have one
-const SITE_URL = 'https://090877cb.portfolio-87a.pages.dev/';
+const SITE_URL = 'https://synthwaveblues.com';
 
 export default defineConfig({
   site: SITE_URL,
